234b3aecf9632651a09a4172a78af104431b3799
ddav118 <ddavilag@ucsd.edu>
2022-10-26 21:23:40 +0000

    added

--- train.py
+++ train.py
@@ -1,14 +1,12 @@
 from tqdm import tqdm
 import numpy as np
 import torch
-# training
 
-def train1Epoch(epoch_index, model, optimizer, loss_fn, training_loader, tb_writer):
+# training
+def train1Epoch(epoch_index, model, optimizer, loss_fn, training_loader, tb_writer=None):
     model.train()
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    running_loss = 0.
-    last_loss = 0.
     losses = np.array([])
 
     # Here, we use enumerate(training_loader) instead of
@@ -16,37 +14,116 @@ def train1Epoch(epoch_index, model, optimizer, loss_fn, training_loader, tb_writ
     # index and do some intra-epoch reporting
     for i, data in tqdm(enumerate(training_loader), total=len(training_loader)):
         # Every data instance is an input + label pair
+        
         image, bnpp = data
         image, bnpp = image.to(device, non_blocking=True), bnpp.to(device, non_blocking=True)
-        image = image.float()
-        bnpp = bnpp.float()
-        #print(image.shape)
+        #bnpp = bnpp.float()
+        
+        # print('image size: ',image.shape)
         #print(torch.mean(image))
-
-        # Zero your gradients for every batch!
+        #HERE
+        
+        pred = model(image)
+        # print('BNPP size: ',bnpp.shape)
+        # print('Pred size: ',pred.squeeze().shape)
+        # print('BNPP: ',bnpp)
+        # print('Preds: ',pred)
+        # print('Preds: ',pred.squeeze())
+        
+        loss = loss_fn(torch.squeeze(pred,1), bnpp)
+        #print('loss: ',loss.item())
+        # Backpropagation
         optimizer.zero_grad()
-
-        # Make predictions for this batch
-        outputs = model(image)
-
-        # Compute the loss and its gradients
-        loss = loss_fn(outputs.squeeze(), bnpp)
         loss.backward()
-        #print(f"{loss=}")
-
-        # Adjust learning weights
         optimizer.step()
+        
+        #HERE
+#         # Zero your gradients for every batch!
+#         optimizer.zero_grad()
+
+#         # Make predictions for this batch
+#         outputs = model(image)
+
+#         # Compute the loss and its gradients
+#         loss = loss_fn(outputs.squeeze(), bnpp)
+#         loss.backward()
+#         #print(f"{loss=}")
+
+#         # Adjust learning weights
+#         optimizer.step()
 
         # Gather data and report
-        running_loss += loss.item()
+        #running_loss += loss.item()
         losses = np.append(losses, loss.item())
+        
         #print(f"{running_loss=}")
-    last_loss += running_loss / (len(training_loader)) #number of batches
+    #last_loss += running_loss / (len(training_loader)) #number of batches
+    #print('last_loss: ',last_loss)
         #if i % 1000 == 0:
         #   last_loss = running_loss / len(training_loader) # loss per batch
             #print('  batch {} loss: {}'.format(i + 1, last_loss))
             #tb_x = epoch_index * len(training_loader) + i + 1
             #tb_writer.add_scalar('Loss/train', last_loss, tb_x)
         #    running_loss = 0.
+    #print('Running Losses: ',losses)
+    return np.mean(losses)
 
+def test1Epoch(epoch_index, model, loss_fn, valid_loader, tb_writer=None):
+    model.eval()
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    losses = np.array([])
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(valid_loader), total=len(valid_loader)):
+            # Every data instance is an input + label pair
+
+            image, bnpp = data
+            image, bnpp = image.to(device, non_blocking=True), bnpp.to(device, non_blocking=True)
+            #bnpp = bnpp.float()
+            #print('BNPP: ',bnpp)
+            #print(torch.mean(image))
+            #HERE
+
+            pred = model(image)
+            #print('BNPP: ',bnpp)
+            #print('Preds: ',torch.squeeze(pred,1))
+            
+            loss = loss_fn(torch.squeeze(pred,1), bnpp).detach()
+            #print('loss: ',loss.item())
+
+            #HERE
+    #         # Zero your gradients for every batch!
+    #         optimizer.zero_grad()
+
+    #         # Make predictions for this batch
+    #         outputs = model(image)
+
+    #         # Compute the loss and its gradients
+    #         loss = loss_fn(outputs.squeeze(), bnpp)
+    #         loss.backward()
+    #         #print(f"{loss=}")
+
+    #         # Adjust learning weights
+    #         optimizer.step()
+
+            # Gather data and report
+            #running_loss += loss.item()
+            losses = np.append(losses, loss.item())
+            image.detach() 
+            bnpp.detach()
+        
+        #print(f"{running_loss=}")
+    #last_loss += running_loss / (len(training_loader)) #number of batches
+    #print('last_loss: ',last_loss)
+        #if i % 1000 == 0:
+        #   last_loss = running_loss / len(training_loader) # loss per batch
+            #print('  batch {} loss: {}'.format(i + 1, last_loss))
+            #tb_x = epoch_index * len(training_loader) + i + 1
+            #tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+        #    running_loss = 0.
+    #print('Running Losses: ',losses)
     return np.mean(losses)
